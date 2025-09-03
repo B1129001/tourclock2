822a0f7a010e60134d222227e60a6baf8b026949
B1129001 <119854519+B1129001@users.noreply.github.com>
2025-09-03 18:35:33 +0800

    Update CalendarGenerator.js

--- src/components/CalendarGenerator.js
+++ src/components/CalendarGenerator.js
@@ -459,7 +459,7 @@ const CalendarGenerator = () => {
 
       <div className="content-wrapper">
         <div className="form-card">
-          <h2>📅 東京澀谷集合活動設定</h2>
+          <h2>📅 集合活動通知</h2>
 
           <div className="form-group">
             <label htmlFor="name">集合名稱</label>
